9fdd607fbaeb036c7db5d0d2220a63d40131afaf
Marenz <github@supradigital.org>
2022-03-15 18:52:59 +0100

    LSP.py: Implement simple send/respond framework

--- test/libsolidity/lsp/goto_definition.sol
+++ test/libsolidity/lsp/goto_definition.sol
@@ -2,13 +2,16 @@
 pragma solidity >=0.8.0;
 
 import "./lib.sol";
+//      ^ @importDirective
 
 interface I
 {
     function f(uint x) external returns (uint);
+    //       ^ @functionF
 }
 
 contract IA is I
+    //   ^^ @IASymbol
 {
     function f(uint x) public pure override returns (uint) { return x + 1; }
 }
@@ -21,6 +24,7 @@ contract IB is I
 library IntLib
 {
     function add(int self, int b) public pure returns (int) { return self + b; }
+    //       ^^^ @IntLibAdd
 }
 
 contract C
@@ -29,40 +33,199 @@ contract C
     function virtual_inheritance() public payable
     {
         obj = new IA();
+        //        ^ @usingIASymbol
         obj.f(1); // goto-definition should jump to definition of interface.
+    //      ^ @virtualFunctionLookup
     }
 
     using IntLib for *;
     function using_for(int i) pure public
     {
         i.add(5);
+ //       ^ @usingIntAdd
         14.add(4);
     }
 
     function useLib(uint n) public payable returns (uint)
     {
         return Lib.add(n, 1);
+        //     ^ @LibSymbol
+        //         ^ @LibAddSymbol
     }
 
     function enums(Color c) public pure returns (Color d)
+    //             ^ @ColorSymbolInParameter
     {
         Color e = Color.Red;
+        //    ^ @eVariableDeclaration
+        //              ^ @RedEnumMemberAccess
         if (c == e)
+        //       ^ @eVariableAccess
             d = Color.Green;
         else
             d = c;
     }
 
     type Price is uint128;
+    //   ^^^^^ @PriceDeclaration
     function udlTest() public pure returns (uint128)
     {
         Price p = Price.wrap(128);
+    //  ^ @PriceSymbol
+    //            ^ @PriceInWrap
         return Price.unwrap(p);
     }
 
     function structCtorTest(uint8 v) public pure returns (uint8 result)
     {
         RGBColor memory c = RGBColor(v, 2 * v, 3 * v);
+        //                       ^ @RGBColorCursor
         result = c.red;
+        int a;
+//      ^^^^^ @unusedLocalVar
     }
 }
+// ----
+// goto_definition: @unusedLocalVar 2072
+// lib: @diagnostics 2072
+// -> textDocument/definition {
+//     "position": @importDirective
+// }
+// <- {
+//     "result": [
+//         {
+//             "range": {
+//                 "end": {
+//                     "character": 0,
+//                     "line": 0
+//                 },
+//                 "start": {
+//                     "character": 0,
+//                     "line": 0
+//                 }
+//             },
+//             "uri": "lib.sol"
+//         }
+//     ]
+// }
+// -> textDocument/definition {
+//     "position": @usingIASymbol
+// }
+// <- {
+//     "result": [
+//         {
+//             "range": @IASymbol,
+//             "uri": "goto_definition.sol"
+//         }
+//     ]
+// }
+// -> textDocument/definition {
+//     "position": @virtualFunctionLookup
+// }
+// <- {
+//     "result": [
+//         {
+//             "range": @functionF,
+//             "uri": "goto_definition.sol"
+//         }
+//     ]
+// }
+// -> textDocument/definition {
+//     "position": @usingIntAdd
+// }
+// <- {
+//     "result": [
+//         {
+//             "range": @IntLibAdd,
+//             "uri": "goto_definition.sol"
+//         }
+//     ]
+// }
+// -> textDocument/definition {
+//     "position": @LibSymbol
+// }
+// <- {
+//     "result": [
+//         {
+//             "range": @LibLibrary,
+//             "uri": "lib.sol"
+//         }
+//     ]
+// }
+// -> textDocument/definition {
+//     "position": @LibAddSymbol
+// }
+// <- {
+//     "result": [
+//         {
+//             "range": @addSymbol,
+//             "uri": "lib.sol"
+//         }
+//     ]
+// }
+// -> textDocument/definition {
+//     "position": @ColorSymbolInParameter
+// }
+// <- {
+//     "result": [
+//         {
+//             "range": @ColorEnum,
+//             "uri": "lib.sol"
+//         }
+//     ]
+// }
+// -> textDocument/definition {
+//     "position": @RedEnumMemberAccess
+// }
+// <- {
+//     "result": [
+//         {
+//             "range": @EnumMemberRed,
+//             "uri": "lib.sol"
+//         }
+//     ]
+// }
+// -> textDocument/definition {
+//     "position": @eVariableAccess
+// }
+// <- {
+//     "result": [
+//         {
+//             "range": @eVariableDeclaration,
+//             "uri": "goto_definition.sol"
+//         }
+//     ]
+// }
+// -> textDocument/definition {
+//     "position": @PriceSymbol
+// }
+// <- {
+//     "result": [
+//         {
+//             "range": @PriceDeclaration,
+//             "uri": "goto_definition.sol"
+//         }
+//     ]
+// }
+// -> textDocument/definition {
+//     "position": @PriceInWrap
+// }
+// <- {
+//     "result": [
+//         {
+//             "range": @PriceDeclaration,
+//             "uri": "goto_definition.sol"
+//         }
+//     ]
+// }
+// -> textDocument/definition {
+//     "position": @RGBColorCursor
+// }
+// <- {
+//     "result": [
+//         {
+//             "range": @RGBColorStruct,
+//             "uri": "lib.sol"
+//         }
+//     ]
+// }
--- test/libsolidity/lsp/goto_definition_imports.sol
+++ test/libsolidity/lsp/goto_definition_imports.sol
@@ -2,18 +2,80 @@
 pragma solidity >=0.8.0;
 
 import {Weather as Wetter} from "./lib.sol";
+//       ^ @wheatherImportCursor
 import "./lib.sol" as That;
+//                    ^^^^ @ThatImport
 
 contract C
 {
     function test_symbol_alias() public pure returns (Wetter result)
+                                             //        ^ @WetterCursor
     {
         result = Wetter.Sunny;
     }
 
     function test_library_alias() public pure returns (That.Color result)
+                                                //     ^ @ThatCursor
     {
         That.Color color = That.Color.Red;
+//      ^ @ThatVarCursor   ^ @ThatExpressionCursor
         result = color;
     }
 }
+// ----
+// lib: @diagnostics 2072
+// -> textDocument/definition {
+//     "position": @wheatherImportCursor
+// }
+// <- {
+//     "result": [
+//         {
+//             "range": @whetherEnum,
+//             "uri": "lib.sol"
+//         }
+//     ]
+// }
+// -> textDocument/definition {
+//     "position": @WetterCursor
+// }
+// <- {
+//     "result": [
+//         {
+//             "range": @whetherEnum,
+//             "uri": "lib.sol"
+//         }
+//     ]
+// }
+// -> textDocument/definition {
+//     "position": @ThatCursor
+// }
+// <- {
+//     "result": [
+//         {
+//             "range": @ColorEnum,
+//             "uri": "lib.sol"
+//         }
+//     ]
+// }
+// -> textDocument/definition {
+//     "position": @ThatVarCursor
+// }
+// <- {
+//     "result": [
+//         {
+//             "range": @ColorEnum,
+//             "uri": "lib.sol"
+//         }
+//     ]
+// }
+// -> textDocument/definition {
+//     "position": @ThatExpressionCursor
+// }
+// <- {
+//     "result": [
+//         {
+//             "range": @ThatImport,
+//             "uri": "goto_definition_imports.sol"
+//         }
+//     ]
+// }
